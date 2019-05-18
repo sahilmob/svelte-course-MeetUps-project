e2e20f486190fb09ae22322a7688240ca9023d08
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-18 14:07:39 +0300

    add dummy meetups array

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,28 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import { codingImageUrl, swimmingImageUrl } from "./constants";
+
+  const meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "In this meetup, we will have some experts that teach you how to code",
+      imageUrl: codingImageUrl,
+      address: "27th Nerd Road, 324325 New York",
+      contactEmail: "code@test.com"
+    },
+    {
+      id: "m2",
+      title: "Swim Together",
+      subtitle: "Let's go for some swimming",
+      description: "We will simply, swim some rounds!",
+      imageUrl: swimmingImageUrl,
+      address: "27th Nerd Road, 324325 New York",
+      contactEmail: "swim@test.com"
+    }
+  ];
 </script>
 
 <Header />
