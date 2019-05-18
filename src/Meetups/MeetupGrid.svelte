04bd3ad6ddbca2e284cf9422549672469ef4d73e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-18 14:42:33 +0300

    init

---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let meetups;
+  import MeetupItem from "./MeetupItem.svelte";
+</script>
+
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
+
+<section class="meetups">
+  {#each meetups as { title, subtitle, description, imageUrl, address, contactEmail }}
+    <MeetupItem
+      {title}
+      {subtitle}
+      {description}
+      {imageUrl}
+      {address}
+      {contactEmail} />
+  {/each}
+</section>
