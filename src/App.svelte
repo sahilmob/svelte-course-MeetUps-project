c9a11d2be8a416cac10224464fbb6d8d7f7ab923
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-18 14:59:11 +0300

    add functionality to insert new meetup

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,14 @@
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import { codingImageUrl, swimmingImageUrl } from "./constants";
 
-  const meetups = [
+  let title = "";
+  let subtitle = "";
+  let imageUrl = "";
+  let address = "";
+  let contactEmail = "";
+  let description = "";
+
+  let meetups = [
     {
       id: "m1",
       title: "Coding Bootcamp",
@@ -24,6 +31,20 @@
       contactEmail: "swim@test.com"
     }
   ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title,
+      subtitle,
+      imageUrl,
+      address,
+      contactEmail,
+      description
+    };
+
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
 <style>
@@ -35,5 +56,33 @@
 <Header />
 
 <main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title} />
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle} />
+    </div>
+
+    <div class="form-control">
+      <label for="imageUrl">Image URL</label>
+      <input type="text" id="imageUrl" bind:value={imageUrl} />
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value={address} />
+    </div>
+    <div class="form-control">
+      <label for="email">Contact Email</label>
+      <input type="email" id="email" bind:value={contactEmail} />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea id="description" rows="3" bind:value={description} />
+    </div>
+    <button type="submit">Save</button>
+  </form>
   <MeetupGrid {meetups} />
 </main>
