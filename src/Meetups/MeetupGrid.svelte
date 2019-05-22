193e137d7cf90c4d106150fd600965548aa54bc5
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-22 20:43:37 +0300

    add fav. filter

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,25 +1,43 @@
 <script>
   export let meetups;
   import MeetupItem from "./MeetupItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
+
+  let favsOnly = false;
+  $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
+  function setFilter(event) {
+    if (event.detail == 1) {
+      favsOnly = true;
+    } else {
+      favsOnly = false;
+    }
+  }
 </script>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  #meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
 </style>
 
-<section class="meetups">
-  {#each meetups as { title, subtitle, description, imageUrl, address, contactEmail, id, isFavorite }, index}
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} {favsOnly} />
+</section>
+<section id="meetups">
+  {#each filteredMeetups as { title, subtitle, description, imageUrl, address, contactEmail, id, isFavorite }, index}
     <MeetupItem
       {id}
       {title}
