3ea8043a2fd54f253a3837d3f445dac3bc5a65dc
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-23 17:46:09 +0300

    fetch meetups from firebase

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,27 @@
   import Header from "./UI/Header.svelte";
   import Button from "./UI/Button.svelte";
   import meetups from "./Meetups/meetups-store";
+  import { FIREBASE_BASE_URL } from "./constants";
 
   let editMode;
   let page = "overview";
   let selectedMeetupId;
+
+  fetch(`${FIREBASE_BASE_URL}/meetups.json`)
+    .then(res => {
+      if (!res.ok) throw new Error("An error occurred, try again later.");
+      return res.json();
+    })
+    .then(data => {
+      const transformedMeetup = Object.keys(data).map(key => {
+        return {
+          ...data[key],
+          id: key
+        };
+      });
+      meetups.setMeetups(transformedMeetup);
+    })
+    .catch(err => console.log(err));
 
   function addMeetup() {
     editMode = null;
